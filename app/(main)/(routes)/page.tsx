71ef8500bcea359533727acb447218f90995723c
ZackMurray89 <zackmurray.dev@gmail.com>
2024-07-26 10:18:04 -0500

    primsa neondb initial setup complete

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,3 +1,11 @@
+import { ModeToggle } from '@/components/mode-toggle'
+import { UserButton } from '@clerk/nextjs'
+
 export default function Home() {
-  return <div>This is a protected route.</div>
+  return (
+    <div className='flex flex-col'>
+      <UserButton />
+      <ModeToggle />
+    </div>
+  )
 }
