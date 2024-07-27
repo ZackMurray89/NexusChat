37bb2a2e63d8a0a9dba774d3d4f0c98381691eac
ZackMurray89 <zackmurray.dev@gmail.com>
2024-07-27 08:48:37 -0500

    modal functionality to create server complete

--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -4,6 +4,7 @@ import * as z from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useForm } from 'react-hook-form'
 import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 import {
   Dialog,
@@ -27,6 +28,7 @@ import { Input } from '@/components/ui/input'
 
 import { Button } from '@/components/ui/button'
 import { FileUpload } from '@/components/file-upload'
+import { useRouter } from 'next/navigation'
 
 const formSchema = z.object({
   name: z.string().min(1, {
@@ -39,6 +41,8 @@ const formSchema = z.object({
 
 export const InitialModal = () => {
   const [isMounted, setIsMounted] = useState(false)
+
+  const router = useRouter()
 
   useEffect(() => {
     setIsMounted(true)
@@ -55,7 +59,15 @@ export const InitialModal = () => {
   const isLoading = form.formState.isSubmitting
 
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
-    console.log(values)
+    try {
+      await axios.post('/api/servers', values)
+
+      form.reset()
+      router.refresh()
+      window.location.reload()
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   if (!isMounted) {
