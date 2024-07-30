43595bc2c7d2d430ca08782756da8a6d6c3cb631
ZackMurray89 <zackmurray.dev@gmail.com>
2024-07-30 08:08:16 -0500

    leave server api route completed

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -79,7 +79,10 @@ export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
         )}
         {isModerator && <DropdownMenuSeparator />}
         {isAdmin && (
-          <DropdownMenuItem className='text-rose-500 px-3 py-2 text-sm cursor-pointer justify-between'>
+          <DropdownMenuItem
+            onClick={() => onOpen('deleteServer', { server })}
+            className='text-rose-500 px-3 py-2 text-sm cursor-pointer justify-between'
+          >
             Delete Server
             <Trash className='h-4 w-4 ml-auto' />
           </DropdownMenuItem>
