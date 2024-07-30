92dfa8ebafdd50f535e324efaf7feb8dd4426afe
ZackMurray89 <zackmurray.dev@gmail.com>
2024-07-30 18:12:07 -0500

    channel sidebar UI complete

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,18 @@
 import type { Metadata } from 'next'
+
 import { Open_Sans } from 'next/font/google'
 import './globals.css'
+
 import { ClerkProvider } from '@clerk/nextjs'
 import { ThemeProvider } from '@/components/providers/theme-provider'
-import { cn } from '@/lib/utils'
 import { ModalProvider } from '@/components/providers/modal-provider'
+
+import { cn } from '@/lib/utils'
 
 const font = Open_Sans({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'NexusChat',
+  title: 'NexusChat 1.0 | A Discord Clone',
   description: 'NexusChat by Zack Murray',
 }
 
--- components/modals/create-channel-modal.tsx
+++ components/modals/create-channel-modal.tsx
@@ -9,6 +9,7 @@ import { useForm } from 'react-hook-form'
 import {
   Dialog,
   DialogContent,
+  DialogDescription,
   DialogFooter,
   DialogHeader,
   DialogTitle,
@@ -36,6 +37,7 @@ import {
   SelectValue,
 } from '@/components/ui/select'
 import { ChannelType } from '@prisma/client'
+import { useEffect } from 'react'
 
 const formSchema = z.object({
   name: z
@@ -53,20 +55,29 @@ const formSchema = z.object({
 })
 
 export const CreateChannelModal = () => {
-  const { isOpen, onClose, type } = useModal()
+  const { isOpen, onClose, type, data = {} } = useModal()
 
   const router = useRouter()
   const params = useParams()
 
   const isModalOpen = isOpen && type === 'createChannel'
+  const { channelType } = data
 
   const form = useForm({
     resolver: zodResolver(formSchema),
     defaultValues: {
       name: '',
-      type: ChannelType.TEXT,
+      type: channelType || ChannelType.TEXT,
     },
   })
+
+  useEffect(() => {
+    if (channelType) {
+      form.setValue('type', channelType)
+    } else {
+      form.setValue('type', ChannelType.TEXT)
+    }
+  }, [channelType, form])
 
   const isLoading = form.formState.isSubmitting
 
