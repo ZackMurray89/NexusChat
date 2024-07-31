dd4823c29e2368b7f32dd00ac39d4ebf2baccd93
ZackMurray89 <zackmurray.dev@gmail.com>
2024-07-31 11:33:38 -0500

    socketIo setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import { ThemeProvider } from '@/components/providers/theme-provider'
 import { ModalProvider } from '@/components/providers/modal-provider'
 
 import { cn } from '@/lib/utils'
+import { SocketProvider } from '@/components/providers/socket-provider'
 
 const font = Open_Sans({ subsets: ['latin'] })
 
@@ -32,8 +33,10 @@ export default function RootLayout({
             storageKey='discord-theme'
             disableTransitionOnChange
           >
-            <ModalProvider />
-            {children}
+            <SocketProvider>
+              <ModalProvider />
+              {children}
+            </SocketProvider>
           </ThemeProvider>
         </body>
       </html>
