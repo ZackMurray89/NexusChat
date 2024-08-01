44b79cb1fd15ac54707cb4a1e09cd54bbcabc0e2
ZackMurray89 <zackmurray.dev@gmail.com>
2024-08-01 09:56:09 -0500

    basis of chat screen complete

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,7 @@ import { ModalProvider } from '@/components/providers/modal-provider'
 
 import { cn } from '@/lib/utils'
 import { SocketProvider } from '@/components/providers/socket-provider'
+import { QueryProvider } from '@/components/providers/query-provider'
 
 const font = Open_Sans({ subsets: ['latin'] })
 
@@ -35,7 +36,7 @@ export default function RootLayout({
           >
             <SocketProvider>
               <ModalProvider />
-              {children}
+              <QueryProvider>{children}</QueryProvider>
             </SocketProvider>
           </ThemeProvider>
         </body>
---
+++ components/chat/chat-messages.tsx
@@ -0,0 +1,69 @@
+'use client'
+
+import { Member } from '@prisma/client'
+import { ChatWelcome } from './chat-welcome'
+import { useChatQuery } from '@/hooks/use-chat-query'
+import { Loader2, ServerCrash } from 'lucide-react'
+
+interface ChatMessagesProps {
+  name: string
+  member: Member
+  chatId: string
+  apiUrl: string
+  socketUrl: string
+  socketQuery: Record<string, string>
+  paramKey: 'channelId' | 'conversationId'
+  paramValue: string
+  type: 'channel' | 'conversation'
+}
+
+export const ChatMessages = ({
+  name,
+  member,
+  chatId,
+  apiUrl,
+  socketUrl,
+  socketQuery,
+  paramKey,
+  paramValue,
+  type,
+}: ChatMessagesProps) => {
+  const queryKey = `chat:${chatId}`
+
+  const { data, fetchNextPage, hasNextPage, isFetchingNextPage, status } =
+    useChatQuery({
+      queryKey,
+      apiUrl,
+      paramKey,
+      paramValue,
+    })
+
+  if (status === 'pending') {
+    return (
+      <div className='flex flex-col flex-1 justify-center items-center'>
+        <Loader2 className='h-7 w-7 text-zinc-500 animate-spin my-4' />
+        <p className='text-xs text-zinc-500 dark:text-zinc-400'>
+          Loading Messages...
+        </p>
+      </div>
+    )
+  }
+
+  if (status === 'error') {
+    return (
+      <div className='flex flex-col flex-1 justify-center items-center'>
+        <ServerCrash className='h-7 w-7 text-zinc-500 my-4' />
+        <p className='text-xs text-zinc-500 dark:text-zinc-400'>
+          Something Went Wrong...
+        </p>
+      </div>
+    )
+  }
+
+  return (
+    <div className='flex-1 flex flex-col py-4 overflow-y-auto'>
+      <div className='flex-1' />
+      <ChatWelcome type={type} name={name} />
+    </div>
+  )
+}
