72b7801870b73c6cf8b3870eaad859cbe5acc2f4
ZackMurray89 <zackmurray.dev@gmail.com>
2024-08-01 11:05:45 -0500

    pdf error

--- components/chat/chat-item.tsx
+++ components/chat/chat-item.tsx
@@ -1,9 +1,10 @@
 'use client'
 
-import { Member, Profile } from '@prisma/client'
+import { Member, MemberRole, Profile } from '@prisma/client'
 import { UserAvatar } from '../user-avatar'
 import { ActionTooltip } from '../action-tooltip'
-import { ShieldAlert, ShieldCheck } from 'lucide-react'
+import { FileIcon, ShieldAlert, ShieldCheck } from 'lucide-react'
+import Image from 'next/image'
 
 interface ChatItemProps {
   id: string
@@ -38,6 +39,15 @@ export const ChatItem = ({
   socketUrl,
   socketQuery,
 }: ChatItemProps) => {
+  const fileType = fileUrl?.split('.').pop()
+  const isAdmin = currentMember.role === MemberRole.ADMIN
+  const isModerator = currentMember.role === MemberRole.MODERATOR
+  const isOwner = currentMember.id === member.id
+  const canDeleteMessage = !deleted && (isAdmin || isModerator || isOwner)
+  const canEditMessage = !deleted && isOwner && !fileUrl
+  const isPDF = fileType === 'pdf' && fileUrl
+  const isImage = !isPDF && fileUrl
+
   return (
     <div className='relative group flex items-center hover:bg-black/5 p-4 transition w-full'>
       <div className='group flex gap-x-2 items-start w-full'>
@@ -58,7 +68,35 @@ export const ChatItem = ({
               {timestamp}
             </span>
           </div>
-          {content}
+          {isImage && (
+            <a
+              href={fileUrl}
+              target='_blank'
+              rel='noopener noreferrer'
+              className='relative aspect-square rounded-md mt-2 overflow-hidden border flex items-center bg-secondary h-48 w-48'
+            >
+              <Image
+                src={fileUrl}
+                alt={content}
+                fill
+                sizes='(max-width: 192px), (max-height: 192px)'
+                className='object-cover'
+              />
+              {isPDF && (
+                <div className='relative flex items-center p-2 mt-2 rounded-md bg-background/10'>
+                  <FileIcon className='h-10 w-10 fill-emerald-200 stroke-emerald-500' />
+                  <a
+                    href={fileUrl}
+                    target='_blank'
+                    rel='noopener nereferrer'
+                    className='ml-2 text-sm text-emerald-500 dark:text-emerald-500 hover:underline'
+                  >
+                    PDF File
+                  </a>
+                </div>
+              )}
+            </a>
+          )}
         </div>
       </div>
     </div>
